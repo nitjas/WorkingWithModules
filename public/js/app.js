f11dbb80ebb3637f7875776d7820b3ab2f838ed1
Anantadyant Singh <anantadyant@gmail.com>
2021-05-30 23:44:59 +0530

    Named Exports

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
 (function () {
-  function getSessions() {
-    return new Promise(function (resolve, reject) {
-      var oReq = new XMLHttpRequest();
-      oReq.onload = function (e) {
-        resolve(e.target.response);
-      };
-      oReq.open("GET", "sessions.json", true);
-      oReq.responseType = "json";
-      oReq.send();
-    });
-  }
-
   function render() {
     var list = document.querySelector("#sessions");
     if (!list) return;
--- public/js/template.js
+++ public/js/template.js
@@ -1,7 +1,7 @@
-export const errorMessage =
+const errorMessage =
   "<p><em>You do not have any list items yet. Try adding one with the form above.</em></p>";
 
-export function applyTemplate(item) {
+function applyTemplate(item) {
   return `<div class="col-xs-12 col-sm-4 col-md-4 adj_text"><h3>${item.title}</h3>
   <p>${item.tagline}</p>
   <img src="${item.img}" alt="${item.alt}" />
@@ -11,9 +11,11 @@ export function applyTemplate(item) {
   </div></div>`;
 }
 
-export function sessionTemplate(sessions) {
+function sessionTemplate(sessions) {
   // If there are no list items
   if (sessions < 1) return errorMessage;
 
   return "" + sessions.map(applyTemplate).join("") + "";
 }
+
+export { sessionTemplate, errorMessage };
